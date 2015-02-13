9b8de3ac1f8bf56919b5d076a2f07174e5e1ddab
Peter Lorenz <lorenz.pet@hotmail.com>
2015-02-13 10:13:14 +0100

    lolooolololol

---
+++ Client.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Client.cpp
+ * Author: jester
+ * 
+ * Created on February 13, 2015, 10:09 AM
+ */
+
+#include "Client.h"
+
+Client::Client() {
+}
+
+Client::Client(const Client& orig) {
+}
+
+Client::~Client() {
+}
+

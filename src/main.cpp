c0cf654f4ba7d3cf16a1c343f52d26a39abc8ad2
LordYuuma <leoprikler@aol.de>
2015-02-13 23:13:57 +0100

    Recreating hello

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,26 @@
-/* 
- * File:   main.cpp
- * Author: jester
- *
- * Created on February 13, 2015, 10:54 AM
- */
-
-#include <cstdlib>
-
-using namespace std;
-
+/* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
- * 
+ * main.cpp
+ * Copyright (C) 2015 A Game of Codes <>
+ *
+ * JACPlusPlus is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JACPlusPlus is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-int main(int argc, char** argv) {
 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
     return 0;
 }
-
